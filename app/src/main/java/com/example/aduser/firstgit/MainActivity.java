e59c60587061c3b088027df33bdf199c03eaae4f
allan <allajacobmathew@gmail.com>
2018-02-12 02:35:57 -0800

    dummy

--- app/src/main/java/com/example/aduser/firstgit/MainActivity.java
+++ app/src/main/java/com/example/aduser/firstgit/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.aduser.firstgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt=findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "test", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
